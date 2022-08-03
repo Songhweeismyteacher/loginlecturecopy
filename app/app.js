ec7dd5639e0dea34d1a90fc0aa6a6ded0681b323
Songhweeismyteacher <102505173+Songhweeismyteacher@users.noreply.github.com>
2022-08-03 16:59:59 +0900

    돔을 이용하여 HTML 객체 저장

--- app/app.js
+++ app/app.js
@@ -10,7 +10,7 @@ const home = require("./src/routes/home");
 //앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-app.use(express.static(`${__dirname})/src/public`));  //${__dirname}--> app.js가 있는 경로 /src/public  이라는 정적 경로를 설정해줌.
+app.use(express.static("src/public"));;  //${__dirname}--> app.js가 있는 경로 /src/public  이라는 정적 경로를 설정해줌.app.use(express.static(src/public));
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드
 
--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,15 @@
 "use strict";
 
-console.log("hello")
+const id = document.querySelector("#id"), // 선택자(html 관련)
+    psword = document.querySelector("#psword"), // 선택자(html 관련)
+    loginBtn = document.querySelector("button"); // button은 하나여서 #안넣음
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+    id: id.value,
+    psword: psword.value,      //  버튼을 클릭하면 로그인과 패스워드창에 입력한 값을 받음
+    };
+    console.log(req);
+}
