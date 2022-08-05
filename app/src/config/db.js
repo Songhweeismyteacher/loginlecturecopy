188cd34945b6727a2c6e3fdfa222558f7a52352c
Songhweeismyteacher <102505173+Songhweeismyteacher@users.noreply.github.com>
2022-08-05 10:22:12 +0900

    환경 변수 등록

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({                                     //데이터 베이스 설정들 기록, 대소문자 주의
-    host: "selalogin.clxyk7thrtmd.ap-northeast-2.rds.amazonaws.com",
-    user: "admin",
-    password: "wjdgns0204",
-    database: "login_",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PSWORD,
+    database: process.env.DB_DATABASE,
 });
 
 db.connect();
 
-module.exports = db;
+module.exports = db;  // 환경 변수 설정  //.env파일에 환경 변수 저장해 놨음 
