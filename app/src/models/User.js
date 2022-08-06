2d0ae879337c830311ffd2d49250fb3a0434de30
Songhweeismyteacher <102505173+Songhweeismyteacher@users.noreply.github.com>
2022-08-06 17:18:28 +0900

    로그인 경고창 오류  고침

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -1,28 +1,26 @@
 "use strict";
 
-const { response } = require("express");
 const UserStorage = require("./UserStorage");
 
 class User {
     constructor(body) {
-    this.body = body;
+        this.body = body;
     }
 
     async login() {
         const client = this.body;
         try {
-            const { id, psword } = await UserStorage.getUserInfo(client.id);
+            const user = await UserStorage.getUserInfo(client.id);
 
-            if (id) {
-                if (id === client.id && psword === client.psword) {
+            if (user) {
+                if (user.id === client.id && user.psword === client.psword) {
                     return { success: true };
                 }
                 return { success: false, msg: "비밀번호가 틀렸습니다." };
             }
-            return { success: false, msg: "존재하지 않는 아이디입니다." };   // 아이디가 서버에 있는지 확인하고 패스워드가 같으면 true 없으면 false, 메시지 출력
-        
+            return { success: false, msg: "존재하지 않는 아이디입니다." };
         }   catch (err) {
-            return { success: false, msg: err };
+            return { success: false, err };
         }
     }
 
@@ -32,9 +30,9 @@ class User {
             const response = await UserStorage.save(client);
             return response;
         }   catch (err) {
-            return { success: false, msg: err };
+            return { success: false, err };
         }
     }
-}  
+}
 
 module.exports = User;
